0ee0c15df9219f5609558cce99ca963ad07a1556
OksanaYeroshenko <o.ieroshenko@gmail.com>
2022-10-21 14:33:49 +0300

    [REG-199][REF]migrate product pricelist to odoo 16

---
+++ addons/regency_shopsite/models/product_product.py
@@ -0,0 +1,29 @@
+import json
+
+from markupsafe import Markup
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    def open_pricelist_rules(self):
+        self.ensure_one()
+        domain = ['|',
+                  '&', ('product_tmpl_id', '=', self.product_tmpl_id.id), ('applied_on', '=', '1_product'),
+                  '&', ('product_id', '=', self.id), ('applied_on', '=', '0_product_variant'),
+                  '&', ('overlay_tmpl_id', '=', self.product_tmpl_id.overlay_tmpl_id.id), ('applied_on', '=', '4_overlay_template')]
+        return {
+            'name': _('Price Rules'),
+            'view_mode': 'tree,form',
+            'views': [(self.env.ref('product.product_pricelist_item_tree_view_from_product').id, 'tree'), (False, 'form')],
+            'res_model': 'product.pricelist.item',
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+            'domain': domain,
+            'context': {
+                'default_product_id': self.id,
+                'default_applied_on': '0_product_variant',
+            }
+        }
