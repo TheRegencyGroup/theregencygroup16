376c5ecaadf75bb19aa34bb6eb7a8a6b265a8216
rymar.roman <rymar.roman>
2022-10-23 19:56:28 +0300

    [REG-76][DEL] removes regency_hotel_template_relation module

--- addons/regency_hotel_template_relation/models/overlay_template.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class OverlayTemplate(models.Model):
-    _inherit = 'overlay.template'
-
-    hotel_ids = fields.Many2many('res.partner', 'hotel_template_rel', 'template_id', 'hotel_id', string='Hotels',
-                                 domain=[('company_type', '=', 'company'), ('contact_type', '=', 'customer')])
