72525d85f9b6d665f8bfefe2a5d272e8c5595d09
Ros <sinvigil@gmail.com>
2022-10-26 19:55:38 +0200

    [REG-182][REF] change to use useService

--- addons/dr_many_tags_link/static/src/js/tags_list.js
+++ addons/dr_many_tags_link/static/src/js/tags_list.js
@@ -2,14 +2,20 @@
 
 import { TagsList } from '@web/views/fields/many2many_tags/tags_list';
 import { patch } from 'web.utils';
+import { useService } from '@web/core/utils/hooks';
 
 patch(TagsList.prototype, 'dr_many_tags_link/static/src/js/tags_list', {
+    setup() {
+        this._super(...arguments)
+        this.action = useService('action')
+    },
+
     onClickAction(resId, resModel, title) {
         return this.getActionForm(resId, resModel, title);
     },
 
     getActionForm(resId, resModel, title) {
-        return this.env.services.action.doAction({
+        return this.action.doAction({
             res_model: resModel,
             res_id: resId,
             name: title,
