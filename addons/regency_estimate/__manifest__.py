1bcead5cb3311a1b7bf9f3239f10e56d570c2a69
maksym <maximkv25@gmail.com>
2022-11-02 14:43:29 +0200

    [REG-228][REF] design improvements

--- addons/regency_estimate/__manifest__.py
+++ addons/regency_estimate/__manifest__.py
@@ -38,6 +38,7 @@
         'wizard/choose_delivery_carrier_view.xml',
         'views/product_views.xml',
         'views/delivery_carrier.xml',
+        'views/portal_templates.xml'
     ],
     'demo': [
         'data/product_demo.xml',
