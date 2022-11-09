ce8a8627262e92912e23aed211d0cb8e2a1db567
OksanaYeroshenko <o.ieroshenko@gmail.com>
2022-11-09 13:04:15 +0200

    [REG-128][REF]corrected after comments

--- addons/regency_estimate/models/fee_type.py
+++ addons/regency_estimate/models/fee_type.py
@@ -7,3 +7,4 @@ class FeeType(models.Model):
 
     name = fields.Char(required=True)
     product_id = fields.Many2one('product.product', domain="[('detailed_type', '=', 'service')]", required=True)
+    fee_value_ids = fields.One2many('fee.value', 'fee_type_id')
