7190650ba8f9ac45695bbf93536339f6e490196a
OksanaYeroshenko <o.ieroshenko@gmail.com>
2022-11-16 12:31:41 +0200

    [REG-360][REF]restrictions on CA and PS

--- addons/consumption_agreement/models/consumption_agreement.py
+++ addons/consumption_agreement/models/consumption_agreement.py
@@ -8,8 +8,7 @@ class ConsumptionAgreement(models.Model):
 
     name = fields.Char(required=True, copy=False, index=True, default=lambda self: _('New'))
     signed_date = fields.Date()
-    partner_id = fields.Many2one('res.partner', domain=[('contact_type', '=', 'customer')], string='Primary Customer',
-                                 required=True)
+    partner_id = fields.Many2one('res.partner', domain=[('contact_type', '=', 'customer')], string='Primary Customer')
     allowed_partner_ids = fields.Many2many('res.partner',  domain=[('contact_type', '=', 'customer')], string="Allowed Customers")
     line_ids = fields.One2many('consumption.agreement.line', 'agreement_id')
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
