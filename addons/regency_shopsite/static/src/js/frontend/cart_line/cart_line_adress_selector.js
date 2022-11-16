5259ee022d639048a7701b514ee9718c56b7ba79
Ros <sinvigil@gmail.com>
2022-11-16 11:07:14 +0100

    [REG-312][WIP] add selection with add_new_address

--- addons/regency_shopsite/static/src/js/frontend/cart_line/cart_line_adress_selector.js
+++ addons/regency_shopsite/static/src/js/frontend/cart_line/cart_line_adress_selector.js
@@ -9,6 +9,10 @@ const dropPrevious = new Concurrency.MutexedDropPrevious();
 
 export class DeliveryAddressCartLine extends Component {
 
+    get solId() { // 'sol' = 'sale order line'
+        return this.props.solData.solId
+    }
+
     get currentDeliveryAddress() {
         return this.props.solData.currentDeliveryAddress
     }
@@ -17,12 +21,37 @@ export class DeliveryAddressCartLine extends Component {
         return this.props.solData.possibleDeliveryAddresses
     }
 
-    async saveDeliveryAddress(ev) {
-        let sale_order_line_id = this.props.solData.solId;
-        let delivery_address_id = ev.target.value;
-        delivery_address_id = (
-            delivery_address_id && (typeof delivery_address_id === 'string' || typeof delivery_address_id === 'number')
-        ) ? Number(delivery_address_id) : false;
+    async processDeliveryAddressChanging(ev) {
+        let selectionTagVal = ev.target.value
+        if (selectionTagVal === 'add_new_address') {
+            let address_name = 'Some Address Name' // TODO REG-312
+            await this.createNewDeliveryAddress(address_name);
+        } else {
+            let deliveryAddressId = (
+                selectionTagVal && (typeof selectionTagVal === 'string' || typeof selectionTagVal === 'number')
+            ) ? Number(selectionTagVal) : false;
+            await this.saveDeliveryAddress(deliveryAddressId);
+        }
+    }
+
+    async createNewDeliveryAddress(address_name) {
+        let sale_order_line_id = this.solId
+        dropPrevious.exec(() => {
+            return rpc.query({
+                route: '/shop/cart/add_new_address',
+                params: {
+                    sale_order_line_id,
+                    address_name, // TODO REG-312 clarify and add other params
+                },
+            }).catch((e) => {
+                alert(e.message?.data?.message || e.toString())
+            })
+        })
+
+    }
+
+    async saveDeliveryAddress(delivery_address_id) {
+        let sale_order_line_id = this.solId
         dropPrevious.exec(() => {
             return rpc.query({
                 route: '/shop/cart/save_delivery_address',
