05600fa5407b78bc3827bc8273be2ea149fd2d6d
Ros <sinvigil@gmail.com>
2022-11-16 15:13:03 +0100

    [REG-223][FIX] default address

--- addons/eq_sale_multi_delivery_address/models/sale_order.py
+++ addons/eq_sale_multi_delivery_address/models/sale_order.py
@@ -25,25 +25,22 @@ class SaleOrderLine(models.Model):
 
     delivery_partner_id = fields.Many2one('res.partner', string="Hotel Name")
     possible_delivery_address_ids = fields.Many2many('res.partner', compute='_compute_possible_delivery_address_id')
-    delivery_address_id = fields.Many2one('res.partner', string='Delivery Address',
+    delivery_address_id = fields.Many2one('res.partner', string='Delivery Address', store=True, readonly=False,
                                           domain="[('id', 'in', possible_delivery_address_ids)]",
-                                          default=lambda self: self._get_default_delivery_address_id())
+                                          compute='_compute_default_delivery_address_id')
 
-    @api.onchange('delivery_partner_id')
+    @api.depends('delivery_partner_id')
     def _compute_possible_delivery_address_id(self):
         for sol in self:
             address_ids = sol.delivery_partner_id.child_ids.filtered(lambda partner: partner.type == 'delivery') \
                           or sol.delivery_partner_id
             sol.possible_delivery_address_ids = [Command.set(address_ids.ids)]
 
-    @api.onchange('delivery_partner_id', 'possible_delivery_address_ids')
+    @api.depends('delivery_partner_id')
     def _compute_default_delivery_address_id(self):
         for sol in self:
-            sol.delivery_address_id = sol._get_default_delivery_address_id()
-
-    def _get_default_delivery_address_id(self):
-        return self.possible_delivery_address_ids.ids[0] if self.possible_delivery_address_ids else False
-
+            default_address = sol.possible_delivery_address_ids.ids[0] if sol.possible_delivery_address_ids else False
+            sol.delivery_address_id = default_address
 
     def _prepare_procurement_values(self, group_id=False):
         res = super()._prepare_procurement_values(group_id)
