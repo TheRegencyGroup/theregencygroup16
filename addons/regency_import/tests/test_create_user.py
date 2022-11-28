fe27ef407306dca143651a9de5243d4a6b9ab987
oksana <o.ieroshenko@gmail.com>
2022-11-28 21:44:18 +0200

    [REG-442][REF]test

---
+++ addons/regency_import/tests/test_create_user.py
@@ -0,0 +1,15 @@
+from odoo.tests import TransactionCase
+
+
+class TestRun(TransactionCase):
+
+    def test_create_portal_user(self):
+        '''
+        After creating a new Portal User its Partner Entity Type by default should be Contact
+        '''
+        user = self.env['res.users'].create({
+            'name': 'New Portal User',
+            'login': 'new@portal.user',
+            'groups_id': self.env.ref('base.group_portal')
+        })
+        self.assertEqual(user.partner_id.entity_type, 'contact')
