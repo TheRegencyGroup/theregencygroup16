4a086594b9e76d78a5a62007fe63f74288fd89ad
oksana <o.ieroshenko@gmail.com>
2022-11-28 15:24:33 +0200

    [REG-383][REF]

--- addons/regency_stock/models/stock_quant.py
+++ addons/regency_stock/models/stock_quant.py
@@ -17,7 +17,10 @@ class StockQuant(models.Model):
                 lambda f: f.picking_type_id.code == 'incoming')
             for picking in incoming_picking_ids:
                 pol = picking.move_ids.purchase_line_id
-                customer_id = pol.customer_id or picking.product_id.product_tmpl_id.allowed_partner_ids[0] or False
+                customer_id = pol.customer_id
                 if customer_id:
                     break
+                allowed_customer_ids = picking.product_id.product_tmpl_id.allowed_partner_ids
+                if allowed_customer_ids:
+                    customer_id = allowed_customer_ids[0]
         return customer_id
