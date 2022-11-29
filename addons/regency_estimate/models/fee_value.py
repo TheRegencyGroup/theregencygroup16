aca6863ef58e59e209750e9da3f0e380b2801641
maksym <maximkv25@gmail.com>
2022-11-29 15:46:07 +0200

    [REG-420][REF] fee per item

--- addons/regency_estimate/models/fee_value.py
+++ addons/regency_estimate/models/fee_value.py
@@ -1,16 +1,16 @@
-from odoo import fields, models, api, _
+from odoo import fields, models, api
 
 
 class FeeValue(models.Model):
     _name = 'fee.value'
     _description = 'Fee Value'
 
-    name = fields.Char(required=True)
     fee_type_id = fields.Many2one('fee.type', required=True)
     value = fields.Float(required=True)
     percent_value = fields.Float()
     price_sheet_line_id = fields.Many2one('product.price.sheet.line', required=True)
     portal_value = fields.Float(compute='_compute_portal_value', store=True)
+    per_item = fields.Boolean()
 
     @api.depends('price_sheet_line_id.product_uom_qty', 'value', 'percent_value', 'price_sheet_line_id.min_quantity')
     def _compute_portal_value(self):
@@ -26,3 +26,7 @@ class FeeValue(models.Model):
     @api.onchange('percent_value')
     def _onchange_percent_value(self):
         self.value = self.price_sheet_line_id.price * self.price_sheet_line_id.min_quantity * self.percent_value / 100
+
+    @api.onchange('per_item')
+    def _onchange_per_item(self):
+        self.percent_value = 0 if self.per_item else self.percent_value
