35c8a9fb174f3a4e5bf4e1e41b767392283714ce
oksana <o.ieroshenko@gmail.com>
2022-12-01 10:27:31 +0200

    [REG-407][ADD]barcode label for Vendor

--- addons/regency_stock/models/mail_template.py
+++ addons/regency_stock/models/mail_template.py
@@ -10,10 +10,11 @@ class MailTemplate(models.Model):
         results = super().generate_email(res_ids, fields)
         for k, v in results.items():
             if v['model'] == 'purchase.order':
-                po_id = [v['res_id']]
-                report = self.env.ref('regency_stock.action_report_purchase_order_barcode')
-                result, report_format = self.env['ir.actions.report']._render_qweb_pdf(report, po_id)
-                result = base64.b64encode(result)
-                report_name = f'Barcode - {self.env["purchase.order"].browse(po_id).name}'
-                v['attachments'].append((report_name, result))
+                po = self.env["purchase.order"].browse(v['res_id'])
+                if po.state == 'purchase':
+                    report = self.env.ref('regency_stock.action_report_purchase_order_barcode')
+                    result, report_format = self.env['ir.actions.report']._render_qweb_pdf(report, po.order_line.ids)
+                    result = base64.b64encode(result)
+                    report_name = f'Barcode - {po.name}'
+                    v['attachments'].append((report_name, result))
         return results
