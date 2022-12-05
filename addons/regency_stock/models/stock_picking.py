d779ce4e9982090093cc6622e18fe6fdc27a3e69
maksym <maximkv25@gmail.com>
2022-12-05 11:09:39 +0200

    [REG-392][REF] receipt package wizard

--- addons/regency_stock/models/stock_picking.py
+++ addons/regency_stock/models/stock_picking.py
@@ -23,11 +23,12 @@ class StockPicking(models.Model):
     def _set_receipt_package_type(self):
         self.ensure_one()
         view_id = self.env.ref('regency_stock.choose_receipt_package_view_form').id
-        context = dict(
-            self.env.context,
-            default_receipt_package_type_id=self.product_id.packaging_ids[0].package_type_id.id if len(
-                self.product_id.packaging_ids) > 1 else self.product_id.packaging_ids.package_type_id.id
-        )
+        context = dict(self.env.context, default_picking_id=self.id)
+        if self.product_id.packaging_ids and len(self.product_id.packaging_ids) == 1:
+            context.update({
+                'default_receipt_package_type_id': self.product_id.packaging_ids.package_type_id.id,
+                'default_weight': self.product_id.packaging_ids.product_id.weight
+            })
         return {
             'name': _('Package Details'),
             'type': 'ir.actions.act_window',
--- addons/regency_stock/wizard/choose_receipt_package.py
+++ addons/regency_stock/wizard/choose_receipt_package.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.tools import float_compare
 
 
 class ChooseReceiptPackage(models.TransientModel):
@@ -7,7 +8,25 @@ class ChooseReceiptPackage(models.TransientModel):
 
     picking_id = fields.Many2one('stock.picking', 'Picking')
     receipt_package_type_id = fields.Many2one('stock.package.type', 'Receipt Package Type', check_company=True)
-    receipt_weight = fields.Float('Shipping Weight')
+    weight = fields.Float('Shipping Weight')
+    qty = fields.Float('Quantity')  # TODO: where need to save in delivery package?
 
     def action_put_in_pack(self):
-        pass
+        picking_move_lines = self.picking_id.move_line_ids
+        if not self.picking_id.picking_type_id.show_reserved and not self.env.context.get('barcode_view'):
+            picking_move_lines = self.picking_id.move_line_nosuggest_ids
+
+        move_line_ids = picking_move_lines.filtered(lambda ml:
+                                                    float_compare(ml.qty_done, 0.0,
+                                                                  precision_rounding=ml.product_uom_id.rounding) > 0
+                                                    and not ml.result_package_id
+                                                    )
+        if not move_line_ids:
+            move_line_ids = picking_move_lines.filtered(lambda ml: float_compare(ml.reserved_uom_qty, 0.0,
+                                                                                 precision_rounding=ml.product_uom_id.rounding) > 0 and float_compare(
+                ml.qty_done, 0.0,
+                precision_rounding=ml.product_uom_id.rounding) == 0)
+
+        delivery_package = self.picking_id._put_in_pack(move_line_ids)
+        if self.weight:
+            delivery_package.shipping_weight = self.weight
