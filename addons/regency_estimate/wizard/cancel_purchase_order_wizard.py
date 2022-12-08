43edd9e4fac7be838be9488a65bff77430f19153
romadjan <sudorenko.romchik@gmail.com>
2022-12-09 01:01:27 +0200

    [REG-467][ADD] po cancellation wizard

--- addons/regency_estimate/wizard/cancel_purchase_order_wizard.py
+++ addons/regency_estimate/wizard/cancel_purchase_order_wizard.py
@@ -6,12 +6,14 @@ class PreviousPricesWizard(models.TransientModel):
     _description = 'Cancel PO Wizard'
 
     text = fields.Text(readonly=True, default='Are you sure you want to cancel the Purchase order?'
-                                              'If yes, please, provide your reason for cancellation:')
-    cancellation_reason = fields.Char(required=True)
+                                              ' If yes, please, provide your reason for cancellation')
+    cancellation_reason = fields.Char(required=True, string='Cancellation Reason')
 
     def cancel(self):
         order = self.env['purchase.order'].browse(self.env.context.get('active_id'))
         order.write({'cancellation_reason': self.cancellation_reason})
-        order.button_cancel()
         order.message_post(body=_('PO was cancelled due to the reason: %s' % order.cancellation_reason))
+        order.write({'state': 'cancel', 'mail_reminder_confirmed': False})
+        order.sudo()._activity_cancel_on_sale()
+
 
