4b95c0a0f8346cc1f7f3da658a5a4f121a873626
maksym <maximkv25@gmail.com>
2022-12-09 16:52:10 +0200

    [REG-466][REF] no relation to config

--- addons/regency_estimate/models/res_config_settings.py
+++ addons/regency_estimate/models/res_config_settings.py
@@ -5,5 +5,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     estimate_manager_id = fields.Many2one('res.users', string='Estimate manager',
-                                          config_parameter='regency.estimate_manager_id',
                                           related='company_id.estimate_manager_id', readonly=False)
