d6d33c235f0d32cc7f319a2d4a4670d57ab7ead1
maksym <maximkv25@gmail.com>
2022-12-12 12:48:30 +0200

    [REG-466][REF] copy followers and inform purchase representative user

--- addons/regency_estimate/controllers/portal.py
+++ addons/regency_estimate/controllers/portal.py
@@ -345,13 +345,26 @@ class CustomerPortal(portal.CustomerPortal):
 
         # Notify
         estimate_id = selected_price_sheet_line_ids.price_sheet_id.estimate_id
-        if estimate_id and estimate_id.estimate_manager_id:
-            sale_order.message_subscribe(partner_ids=estimate_id.estimate_manager_id.partner_id.ids,
-                                         subtype_ids=[request.env.ref('mail.mt_activities').id,
-                                                      request.env.ref('mail.mt_comment').id])
-            msg = accept_format_string(SystemMessages.get('M-011'), estimate_id.estimate_manager_id.partner_id.name,
-                                       sale_order.name)
-            sale_order.message_post(body=msg, partner_ids=estimate_id.estimate_manager_id.partner_id.ids)
+        if estimate_id:
+            partners_to_subscribe = selected_price_sheet_line_ids.price_sheet_id.message_follower_ids.mapped('partner_id')
+            partners_to_inform = request.env['res.partner']
+            if estimate_id.estimate_manager_id:
+                partners_to_inform += estimate_id.estimate_manager_id.partner_id
+                partners_to_subscribe += estimate_id.estimate_manager_id.partner_id
+            if estimate_id.purchase_agreement_ids:
+                for partner in estimate_id.purchase_agreement_ids.mapped('user_id.partner_id'):
+                    partners_to_subscribe += partner
+                    partners_to_inform += partner
+
+            if partners_to_subscribe:
+                sale_order.message_subscribe(partner_ids=partners_to_subscribe.ids,
+                                              subtype_ids=[request.env.ref('mail.mt_activities').id,
+                                                           request.env.ref('mail.mt_comment').id])
+
+            if partners_to_inform:
+                for partner in partners_to_inform:
+                    msg = accept_format_string(SystemMessages.get('M-011'), partner.name, sale_order.name)
+                    sale_order.message_post(body=msg, partner_ids=partner.ids)
 
         return {
             'force_refresh': True,
@@ -384,15 +397,29 @@ class CustomerPortal(portal.CustomerPortal):
             raise UserError('Orders not found.')
 
         consumption = order_sudo.create_consumption_agreement(selected_price_sheet_line_ids)
+
         # Notify
         estimate_id = selected_price_sheet_line_ids.price_sheet_id.estimate_id
-        if estimate_id and estimate_id.estimate_manager_id:
-            consumption.message_subscribe(partner_ids=estimate_id.estimate_manager_id.partner_id.ids,
-                                         subtype_ids=[request.env.ref('mail.mt_activities').id,
-                                                      request.env.ref('mail.mt_comment').id])
-            msg = accept_format_string(SystemMessages.get('M-011'), estimate_id.estimate_manager_id.partner_id.name,
-                                       consumption.name)
-            consumption.message_post(body=msg, partner_ids=estimate_id.estimate_manager_id.partner_id.ids)
+        if estimate_id:
+            partners_to_subscribe = selected_price_sheet_line_ids.price_sheet_id.message_follower_ids.mapped('partner_id')
+            partners_to_inform = request.env['res.partner']
+            if estimate_id.estimate_manager_id:
+                partners_to_inform += estimate_id.estimate_manager_id.partner_id
+                partners_to_subscribe += estimate_id.estimate_manager_id.partner_id
+            if estimate_id.purchase_agreement_ids:
+                for partner in estimate_id.purchase_agreement_ids.mapped('user_id.partner_id'):
+                    partners_to_subscribe += partner
+                    partners_to_inform += partner
+
+            if partners_to_subscribe:
+                consumption.message_subscribe(partner_ids=partners_to_subscribe.ids,
+                                              subtype_ids=[request.env.ref('mail.mt_activities').id,
+                                                           request.env.ref('mail.mt_comment').id])
+
+            if partners_to_inform:
+                for partner in partners_to_inform:
+                    msg = accept_format_string(SystemMessages.get('M-011'), partner.name, consumption.name)
+                    consumption.message_post(body=msg, partner_ids=partner.ids)
 
         query_string = f'&comeback_url_caption={order_sudo.name}&comeback_url={order_sudo.get_portal_url()}'
 
