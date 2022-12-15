9e33509c2045d2b1eee94bf79cb41d6acc472476
chupryna <happy4Upa@gmail.com>
2022-12-15 18:00:52 +0200

    [REG-543][FIX] check partner in test

--- addons/regency_estimate/tests/test_crm_to_estimate.py
+++ addons/regency_estimate/tests/test_crm_to_estimate.py
@@ -8,8 +8,10 @@ class TestRun(crm_common.TestCrmCommon):
     def setUpClass(cls):
         super(TestRun, cls).setUpClass()
         cls.stage_gen_won.generate_estimate = True
+        cls.lead_1.partner_id = cls.contact_2
 
     def test_opportunity_to_estimate(self):
         self.lead_1.action_set_won()
         self.assertEqual(1, self.lead_1.estimates_count)
         self.assertEqual('draft', self.lead_1.estimate_ids.state)
+        self.assertEqual(self.contact_2, self.lead_1.estimate_ids.partner_id)
