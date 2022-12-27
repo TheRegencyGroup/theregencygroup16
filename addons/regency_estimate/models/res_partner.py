83b00c8ee6824159cca0b5dfc7b69541530046b6
romadjan <sudorenko.romchik@gmail.com>
2022-12-27 11:03:49 +0200

    Revert "[REG-568][FIX] Default Customer contact type"
    This reverts commit 24495499

--- addons/regency_estimate/models/res_partner.py
+++ addons/regency_estimate/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
@@ -73,13 +73,3 @@ class ResPartner(models.Model):
         action['views'] = [x for x in action['views'] if False not in x]
         return action
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            contact_type = self.env.context.get('contact_type')
-            is_company = self.env.context.get('is_company')
-            if is_company:
-                vals['is_company'] = is_company
-            if contact_type:
-                vals['contact_type'] = contact_type
-        return super(ResPartner, self).create(vals_list)
